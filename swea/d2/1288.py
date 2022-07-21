8ffdf156de31afdfa7e181f92cd8d73afe1706e0
rhoeunbin <rhoeb21@gmail.com>
2022-07-22 00:04:06 +0900

    SWEA d2

--- swea/d2/1288.py
+++ swea/d2/1288.py
@@ -7,15 +7,17 @@ for i in range(1, t+1):
     #set에 계속 추가 예정
     numbers = set()
     #while 반복 -> set의 길이가 10이 될 때까지
-    while len(numbers) < 10 :
+    while True :
     #반복할 때 나오는 숫자
     #for 반복 : 숫자를 문자로
         for n in str(N) :
     #numbers set에 추가
             numbers.add(n)
-        print(n, numbers)
+        if len(numbers) == 10 :
+            break
+        
         N += N1
-    print(f'#{t} {N}')
+    print(f'#{i} {N}')
 
 # 1. 반복 ? 모든 수에 체크 => while
 # 2. 모든 수 체크를 어떻게 할 건데??
