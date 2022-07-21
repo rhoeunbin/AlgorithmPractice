996254c65b033a03a6e5b1120718361434aea0da
EunBin <108652504+rhoeunbin@users.noreply.github.com>
2022-07-21 13:54:00 +0900

    Delete swea directory

--- swea/2050.py
+++
@@ -1,5 +0,0 @@
-alp = list(input())  #알파벳을 리스트에 넣어줌
-
-for i in range(len(alp)) :
-    result = (ord(alp[i])-64)  #대문자 A가 65이기 때문에 64를 빼면 1부터 출력
-    print(result,end=' ')
--- swea/2070.py
+++
@@ -1,9 +0,0 @@
-t=int(input())
-for i in range(1,t+1) : 
-    a,b = map(int, input().split())
-    if a==b :
-        print(f'#{i} =')
-    if a>b : 
-        print(f'#{i} >')
-    if a<b :
-        print(f'#{i} <')
